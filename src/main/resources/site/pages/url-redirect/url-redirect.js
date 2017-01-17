bace1e8f1abaa6f6db30024d94fa1dc7273288de
Bobby <bellfalasch@gmail.com>
2017-01-17 12:08:09 +0100

    Deactivate redirects when view content in Content Studio

--- src/main/resources/site/pages/url-redirect/url-redirect.js
+++ src/main/resources/site/pages/url-redirect/url-redirect.js
@@ -1,20 +1,21 @@
 var portal = require('/lib/xp/portal');
 
-// Handle GET request
-exports.get = handleGet;
-
-function handleGet() {
+exports.get = function(req) {
     var result = portal.getContent();
     var url = result.data.url;
 
     var response = {};
 
+	 // Disable the redirect when inside Content Studio
+	 if (req.mode === 'edit' || req.mode === 'preview') {
+		 url = false;
+	 }
+
     if (url) {
         response.redirect = url;
-    }
-    else {
-        response.body = 'No URL configured.'
+    } else {
+        response.body = 'No URL configured (redirects are always disabled when inside Content Studio).';
     }
 
     return response;
-}
+};
