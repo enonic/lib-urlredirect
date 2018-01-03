9120f28390edabfec6e728d1e106a69d2a9a12ab
Bellfalasch <bellfalasch@gmail.com>
2018-01-03 13:10:14 +0100

    Get the rendering fixed

--- src/main/resources/lib/enonic/url-redirect/index.js
+++ src/main/resources/lib/enonic/url-redirect/index.js
@@ -5,17 +5,14 @@ var libs = {
 exports.get = function(req) {
     var result = libs.portal.getContent();
     var url = result.data.url || false;
-	var disabled = !(req.mode === 'edit' || req.mode === 'preview'); // Disable the redirect when inside Content Studio
-    var response = {
-		postProcess: false,
-		applyFilters: false
-	};
+	var disabled = (req.mode === 'edit' || req.mode === 'preview'); // Disable the redirect when inside Content Studio
+    var response = {};
 
-    if (url) {
+    if (url && !disabled) {
         response.redirect = url;
     } else {
 		response.contentType = 'text/html';
-        response.body = '<html><head><title>Disabled</title>' +
+        var body = '<html><head><title>Disabled</title>' +
 						'<style>' +
 						'html,body {min-height:100%;}' +
 						'p {' +
@@ -30,12 +27,13 @@ exports.get = function(req) {
 						'</head><body>' +
 						'<p>';
 		if (disabled) {
-			response.body += 'Redirect created, but has been disabled inside Content Studio.<br />Configured redirect: <a href="' + url + '" target="_blank">' + url + '</a>';
+			body += 'Redirect created, but has been disabled inside Content Studio.<br />Configured redirect: <a href="' + url + '" target="_blank">' + url + '</a>';
 		} else {
-			response.body += 'No URL configured (edit this content in Content Studio).';
+			body += 'No URL configured (edit this content in Content Studio).';
 		}
-		response.body += '</p>';
-		response.body += '</body></html>';
+		body += '</p>';
+		body += '</body></html>';
+		response.body = body;
     }
 
     return response;
