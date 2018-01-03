18d2d6bb7e2e63c017aa6bbb746ca3686df1a508
Bellfalasch <bellfalasch@gmail.com>
2018-01-03 13:15:12 +0100

    Tune the styling on the page text in CS

--- src/main/resources/lib/enonic/url-redirect/index.js
+++ src/main/resources/lib/enonic/url-redirect/index.js
@@ -6,7 +6,10 @@ exports.get = function(req) {
     var result = libs.portal.getContent();
     var url = result.data.url || false;
 	var disabled = (req.mode === 'edit' || req.mode === 'preview'); // Disable the redirect when inside Content Studio
-    var response = {};
+    var response = {
+		applyFilters: false,
+		postProcess: false
+	};
 
     if (url && !disabled) {
         response.redirect = url;
@@ -22,12 +25,13 @@ exports.get = function(req) {
 						'	height: 100%;' +
 						'	display: flex;' +
 						'	text-align: center;' +
+						'	align-items: center;' +
 						'}' +
 						'</style>' +
 						'</head><body>' +
 						'<p>';
-		if (disabled) {
-			body += 'Redirect created, but has been disabled inside Content Studio.<br />Configured redirect: <a href="' + url + '" target="_blank">' + url + '</a>';
+		if (url) {
+			body += 'Redirect created, but has been disabled inside Content Studio.</p><p>Configured redirect: <a href="' + url + '" target="_blank">' + url + '</a>';
 		} else {
 			body += 'No URL configured (edit this content in Content Studio).';
 		}
